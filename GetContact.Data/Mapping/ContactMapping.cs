4994a54b5aafd759c48aef725331b864c329b2a2
rodrigo mahlow <rmahlow@gmail.com>
2025-02-26 22:01:26 -0300

    teste

---
+++ GetContact.Data/Mapping/ContactMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetContact.Domain.Entities;
+
+namespace GetContact.Data.Mapping
+{  
+    public class ContactMapping : IEntityTypeConfiguration<Contatos>
+    {
+        public void Configure(EntityTypeBuilder<Contatos> builder)
+        {
+            builder.HasKey(x => x.IdContato);
+
+            builder.HasIndex(X => X.Email).IsUnique();
+
+            builder.Property(x => x.Nome).HasColumnType("varchar(500)");
+
+            builder.ToTable("TAB_CONTATOS");
+        }
+    }
+}
